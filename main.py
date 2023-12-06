03078a50b8302e11d1715b1c62db08dd6dc49b95
Paulo Henrique Serafim <phls321@hotmail.com>
2023-12-05 22:35:37 -0300

    Ajustes

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 import nltk
 from nltk.tokenize import RegexpTokenizer
 from unidecode import unidecode
+import requests
+from bs4 import BeautifulSoup
 
+def buscarSinonimo(busca):
+    listaSinonimos = []
+    url = "https://www.lexico.pt/" + busca
+    data = requests.get(url)
+    soup = BeautifulSoup(data.content, 'html.parser')
+    conts = soup.find_all('div', class_=["card card-pl"])
+    tags = soup.find_all('p', class_=["text words-buttons"])
+
+    if (tags.__len__() > 1):
+        sinonimos = tags[0].select('span a')
+        for palavra in sinonimos:
+            listaSinonimos.append(palavra.get_text())
+    else:
+        if conts and conts[0]:
+            titulo_element = conts[0].find('h2', class_=["card-title"])
+            if titulo_element:
+                titulo = titulo_element.get_text()
+                lista = conts[0].select('span a')
+                if 'Sinonimo' in titulo or 'Sinónimos' in titulo :
+                    for item in lista:
+                        listaSinonimos.append(unidecode(item.get_text()))
+    return listaSinonimos
+        
 def similaridade(text1, text2):
     f = open(text1, "r", encoding="utf-8")
     texto1 = f.read()
@@ -34,13 +59,23 @@ def similaridade(text1, text2):
     print(key_word_token01)
     print(key_word_token02)
     contador = 0
+    buscar = True
 
     for token in key_word_token01:
-        print(token)
+        buscar = True
         for token2 in key_word_token02:
-            print(token2)
-            if(token == token2):
+            if(token.lower() == token2.lower()):
+                buscar = False
                 contador +=25
+
+        if buscar:
+            lista = buscarSinonimo(token)
+            print(lista)
+            for token in lista:
+                for token2 in key_word_token02:
+                    if(token.lower() == token2.lower()):
+                        contador +=12.5
+
 
     print("A similaridade é de " + str(contador) +  "%")
 
